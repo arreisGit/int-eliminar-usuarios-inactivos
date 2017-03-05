82ea15ffcc80d6b068822924c208b6cf155eba8c
Enrique Sierra Gutierrez <enrique.sierra.0000@gmail.com>
2017-03-05 14:34:52 -0600

    Añade el cuerpo del procedimiento encargado de eliminar las sesiones inactivas

--- SQL/Stored Procedures/EMP_SPD_TerminarSesionesInactivas.sql
+++ SQL/Stored Procedures/EMP_SPD_TerminarSesionesInactivas.sql
@@ -1,7 +1,7 @@
 SET ANSI_NULLS ON
 GO
 
-SET QUOTED_IDENTIFIER ON
+SET QUOTED_IDENTIFIER OFF
 GO
 
 /**************** DROP IF EXISTS ****************/
@@ -21,8 +21,9 @@ END
 GO
 
 /* =============================================
+
   Created by:    Enrique Sierra Gutiérrez.
-  Creation Date: 2017-03-04
+  Creation Date: 2017-03-05
 
   Description: Procedimiento almacenado 
   encargado de eliminar las sesiones de usuario
@@ -31,6 +32,98 @@ GO
 
 ============================================= */
 
-CREATE PROCEDURE [dbo].EMP_SPD_TerminarSesionesInactivas()                
-AS BEGIN 
+CREATE PROCEDURE [dbo].EMP_SPD_TerminarSesionesInactivas               
+AS
+BEGIN 
+  DECLARE
+    @HOY DATETIME,
+    @r_acceso_id INT,
+    @r_spid INT,
+    @query NVARCHAR(500)
+
+  SET @HOY = GETDATE()
+  
+  DECLARE cr_SesionesInactivas CURSOR LOCAL FAST_FORWARD 
+  FOR
+  SELECT DISTINCT
+    acceso.ID,
+    acceso.SPID
+  FROM 
+    Usuario u
+  JOIN Acceso ON  Acceso.Usuario = u.Usuario
+              AND Acceso.FechaRegistro IS NOT NULL
+              AND Acceso.FechaSalida IS NULL
+  JOIN sys.sysprocesses sys_procs ON sys_procs.SPID = Acceso.SPID
+  JOIN master.dbo.IntelisisET et ON et.Empresa = Acceso.Empresa
+                                AND et.Sucursal = Acceso.Sucursal
+                                AND et.Usuario = Acceso.Usuario
+  OUTER APPLY(
+              SELECT TOP 1              
+                [Time] = st.last_execution_time,
+                [Query] = sql_text.TEXT,
+                [DB_NAME] = DB_NAME(qp.dbid)
+              FROM
+                sys.dm_exec_query_stats AS st
+              CROSS APPLY sys.dm_exec_sql_text(st.sql_handle) AS sql_text
+              LEFT JOIN sys.dm_exec_cached_plans cp ON cp.plan_handle = st.plan_handle
+              OUTER APPLY sys.dm_exec_query_plan(cp.plan_handle) AS qp
+              WHERE
+              qp.dbid = sys_procs.dbid
+              AND dbo.RegExIsMatch('\/\*\s' 
+                                  + LTRIM(RTRIM(u.Usuario))
+                                  + '\s.*\*\/'
+                                  , sql_text.TEXT
+                                  , 3) = 1
+              ORDER BY
+                st.last_execution_time DESC
+              ) last_batch
+  -- calc
+  CROSS APPLY(
+              SELECT 
+                MinutosInactivo = DATEDIFF(minute, last_batch.[Time], GETDATE())
+              ) calc
+  WHERE 
+    u.EMP_MinutosMaximosDeInactividad IS NOT NULL
+  AND sys_procs.status = 'sleeping'
+  AND sys_procs.blocked = 0
+  AND calc.MinutosInactivo > u.EMP_MinutosMaximosDeInactividad
+
+  OPEN cr_SesionesInactivas
+
+  FETCH NEXT FROM cr_SesionesInactivas
+  INTO @r_acceso_id, @r_spid
+  
+  WHILE @@fetch_status = 0
+  BEGIN
+    
+    SET @query =  
+"BEGIN TRY 
+  KILL " + CAST(@r_spid AS VARCHAR(5))+ "
+END TRY 
+BEGIN CATCH
+  PRINT
+  (  
+      'Unable to kill the SPID "
+    + CAST(@r_spid AS VARCHAR(5)) + "'
+    + '. Error: ' + CAST(ERROR_NUMBER() AS VARCHAR)
+    + ', Message: ' + ERROR_MESSAGE()
+    + CHAR(13)
+  )
+END CATCH"
+  
+    EXECUTE sp_executesql @query
+    
+    UPDATE Acceso
+    SET FechaSalida = @HOY
+    WHERE ID = @r_acceso_id
+      
+    FETCH NEXT FROM cr_SesionesInactivas
+    INTO @r_acceso_id, @r_spid
+
+  END  
+  
+  CLOSE cr_SesionesInactivas
+
+  DEALLOCATE cr_SesionesInactivas
+
 END
